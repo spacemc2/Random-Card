f532b5dcde7fa7b2b4fbe98e0d44edf5e8368b50
Miguel Torres <m.torres.940106@gmail.com>
2023-02-20 05:52:04 +0000

    random card finished

--- src/app.js
+++ src/app.js
@@ -7,5 +7,75 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  //////////////////////////////////////////////////////////////////////
+
+  let inPuts = [];
+  document
+    .querySelectorAll(".form-range")[0]
+    .addEventListener("input", event => {
+      inPuts[0] = event.target.value;
+    });
+  document
+    .querySelectorAll(".form-range")[1]
+    .addEventListener("input", event => {
+      inPuts[1] = event.target.value;
+      console.log(inPuts);
+    });
+
+  document.querySelector(".createDim").addEventListener("click", () => {
+    document.querySelectorAll(".icon").forEach(elem => {
+      elem.style.fontSize = `${inPuts[0] * 0.15}px`;
+    });
+    document.querySelector(".myfont").style.fontSize = `${inPuts[0] * 0.25}px`;
+
+    document.querySelector(".myroundedborders").style.height = `${inPuts[0]}px`;
+    document.querySelector(".myroundedborders").style.width = `${inPuts[1]}px`;
+  });
+  ////////////////////////////////////////////////////////////////////////
+  function changer() {
+    let ranNum = Math.floor(Math.random() * 13 + 1);
+
+    let lt = document.querySelector(".myfont");
+    if (ranNum == 1) {
+      lt.innerHTML = "A";
+    } else if (ranNum == 11) {
+      lt.innerHTML = "J";
+    } else if (ranNum == 12) {
+      lt.innerHTML = "Q";
+    } else if (ranNum == 13) {
+      lt.innerHTML = "K";
+    } else {
+      lt.innerHTML = String(ranNum);
+    }
+    ///////////////////////////////////////////////////////////////////////////////////
+    let icons = ["♦", "♥", "♠", "♣"];
+    let ranNunIcons = Math.floor(Math.random() * 4);
+
+    document.querySelectorAll(".icon").forEach(elem => {
+      if (ranNunIcons == 0 || ranNunIcons == 1) {
+        elem.firstElementChild.firstElementChild.className = "text-danger px-2";
+      } else {
+        elem.firstElementChild.firstElementChild.className = "px-2";
+      }
+      elem.firstElementChild.firstElementChild.innerHTML = icons[ranNunIcons];
+    });
+  }
+  ///////////////////////////////////////////////////////////////////////////////////////
+  document.querySelector("button").addEventListener("click", () => {
+    changer();
+  });
+
+  var intervalId = window.setInterval(function() {
+    let counter = document.querySelector(".progress").firstElementChild;
+    let perc = Number(counter.style.width.split("%")[0]);
+    counter.style.width = `${perc + 1}%`;
+    counter.innerHTML = `${perc + 1}%`;
+    if (perc == 99) {
+      changer();
+
+      counter.style.width = `${0}%`;
+      counter.innerHTML = `${0}%`;
+    }
+  }, 100);
 };
